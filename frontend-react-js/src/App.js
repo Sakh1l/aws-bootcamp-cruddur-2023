8091b6ceabdb6dfc0d38bccaf21868ecd023ff09
Sakh1l <sakhil2014@gmail.com>
2023-03-01 04:36:33 +0000

    spelling error in notification fix

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
     element: <HomeFeedPage />
   },
   {
-    path: "/notificaitons",
+    path: "/notifications",
     element: <NotificationsFeedPage />
   },
   {
